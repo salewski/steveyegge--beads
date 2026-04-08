8ec8f705b13eec44ea35a3a220060de835b52736
m4dc4p <jgbailey@gmail.com>
2026-04-07 20:07:07 -0700

    Update nix install for v1.0.0 (#3064)
    * Beads / dolt requires ICU libraries and headers for https://github.com/dolthub/go-icu-regex - added
    * Refactor nix slightly for ergonomics.
    * Remove unnecessary overlay

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@
   self,
   buildGoModule,
   git,
+  icu,
   ...
 }:
 buildGoModule {
@@ -16,25 +17,31 @@ buildGoModule {
   doCheck = false;
 
   # Go module dependencies hash - if build fails with hash mismatch, update with the "got:" value
-  vendorHash = "sha256-1BJsEPP5SYZFGCWHLn532IUKlzcGDg5nhrqGWylEHgY=";
+  vendorHash = "sha256-7eb7u47f4/OCnK/T56Zd6b5XUyV6vkBmissryBxANBU=";
 
   # Relax go.mod version for Nix: nixpkgs Go may lag behind the latest
   # patch release, and GOTOOLCHAIN=auto can't download in the Nix sandbox.
   postPatch = ''
     goVer="$(go env GOVERSION | sed 's/^go//')"
     go mod edit -go="$goVer"
+
+    env
   '';
 
   # Allow patch-level toolchain upgrades when a dependency's minimum Go patch
   # version is newer than nixpkgs' bundled patch version.
   env.GOTOOLCHAIN = "auto";
+  # Due to https://github.com/dolthub/go-icu-regex, which requires
+  # separate install of icu headers and library.
+  env.CGO_CPPFLAGS="-I${icu.dev}/include";
+  env.CGO_LDFLAGS="-L${icu}/lib";
 
   # Git is required for tests
   nativeBuildInputs = [ git ];
 
   meta = with lib; {
     description = "beads (bd) - An issue tracker designed for AI-supervised coding workflows";
-    homepage = "https://github.com/steveyegge/beads";
+    homepage = "https://github.com/gastownhall/beads";
     license = licenses.mit;
     mainProgram = "bd";
     maintainers = [ ];
